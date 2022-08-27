286526ea0fbe08d07909b8c35f5b2898c201b38e
Hiago Prates <hiagohps360@gmail.com>
2022-08-27 17:22:43 -0300

    feat: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+import {Wrapper} from './styles'
+
+export function Header() {
+  return (
+    <Wrapper>
+      <img src="./logo.svg" alt="Logo nextcart" />
+    </Wrapper>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #FFFFFF;
+
+  width: 100%;
+  height: 157px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 107px;
+  }
+`;
