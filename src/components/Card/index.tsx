e3da4fbaad76e674479f0afa3b2016f26b14be4f
Hiago Prates <hiagohps360@gmail.com>
2022-08-28 03:46:46 -0300

    feat: modal card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { Avatar, ContentCard, IconUpdate, TextCard, TitleCard, Wrapper } from './styles'; 
+import { Arrow, Avatar, ContentCard, IconUpdate, Name, TextCard, TextHeader, TitleCard, Wrapper, WrapperModal } from './styles'; 
 
 type Props = {
   name: string;
@@ -11,15 +11,26 @@ type Props = {
 
 const Card: FC<Props> = ({name, inputDate, finalDate, avatar}) => {
   return (
-    <Wrapper>
-      <Avatar src={avatar} />
-      <ContentCard>
-        <TitleCard>{name}</TitleCard>
-        <TextCard>{inputDate}</TextCard>
-        <TextCard>{finalDate}</TextCard>
-      </ContentCard>
-      <IconUpdate className="logo" />
-    </Wrapper>
+    <>
+     
+      <Wrapper>
+        <WrapperModal className="modal">
+          <TextHeader>Informações adicionais</TextHeader>
+          <Name>Hiago Prates</Name>
+          <Name>123456789</Name>
+          <Arrow />
+        </WrapperModal>
+        
+        <Avatar src={avatar} />
+        <ContentCard>
+          <TitleCard>{name}</TitleCard>
+          <TextCard>{inputDate}</TextCard>
+          <TextCard>{finalDate}</TextCard>
+        </ContentCard>
+        <IconUpdate className="logo" />
+      </Wrapper>
+    </>
+
   )
 };
 
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -32,6 +32,10 @@ export const Wrapper = styled.div`
     span {
       color: #2F3A58;
     }
+
+    div.modal {
+      display: flex;
+    }
   }
 `;
 
@@ -71,4 +75,56 @@ export const IconUpdate = styled.img.attrs({
   src: './update.svg'
 })`
   margin-left: auto;
+`;
+
+export const WrapperModal = styled.div`
+  display: none;
+
+  position: absolute;
+  width: 12rem;
+
+  background-color: #fff;
+  margin-left: -14.5rem;
+
+  margin-top: 0.2rem;
+
+  flex-direction: column;
+
+  border-radius: 8px;
+`;
+
+export const TextHeader = styled.a`
+  width: 100%;
+  font-weight: bold;
+  font-size: 0.9rem;
+
+  background-color: #50E485;
+
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+
+  padding-left: 0.6rem;
+`;
+
+export const Name = styled.span`
+  margin: 0.3rem 1.2rem 0;
+  font-weight: 300;
+
+  & + & {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const Arrow = styled.div`
+  background-color: #fff;
+
+  width: 16px;
+  height: 16px;
+
+  position: absolute;
+  right: -6px;
+
+  margin-top: 26px;
+
+  transform: rotate(45deg); /* Equal to rotateZ(45deg) */
 `;
