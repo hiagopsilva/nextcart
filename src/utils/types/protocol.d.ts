b2c796125de6e85553f680b7d18c50ac270a9029
Hiago Prates <hiagohps360@gmail.com>
2022-08-28 14:31:55 -0300

    feat: types protocol

---
+++ src/utils/types/protocol.d.ts
@@ -0,0 +1,12 @@
+declare namespace Protocol {
+  declare type List = {
+    _id: string,
+    protocolo: string,
+    apresentante: string,
+    tipo: string,
+    imagem: string,
+    entrada: number,
+    vencimento: number
+  };
+
+}
